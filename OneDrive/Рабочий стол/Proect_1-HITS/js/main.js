b85d510ebab0104644303c315cb2f29ab50529d1
Уйгулаан <kugasovujgulaan@gmail.com>
2026-03-02 20:37:48 +0700

    added drug & drop for blocks

--- OneDrive/Рабочий стол/Proect_1-HITS/js/main.js
+++ OneDrive/Рабочий стол/Proect_1-HITS/js/main.js
@@ -151,3 +151,55 @@ function renderBlockContent(type) {
     `;
   }
 }
+
+function createWorkspaceBlock(type) {
+  const el = document.createElement("div");
+  el.className = "ws-block";
+  el.dataset.type = type;
+
+  el.innerHTML = renderBlockContent(type);
+
+  el.draggable = true;
+
+  return el;
+}
+
+let draggedBlock = null;
+
+// Когда начинаем тащить блок внутри workspace
+workspace.addEventListener("dragstart", e => {
+  if (e.target.classList.contains("ws-block")) {
+    draggedBlock = e.target;
+    e.dataTransfer.effectAllowed = "move";
+  }
+});
+
+// Пока тащим над workspace
+workspace.addEventListener("dragover", e => {
+  e.preventDefault();
+
+  const afterElement = getDragAfterElement(workspace, e.clientY);
+
+  if (afterElement == null) {
+    workspace.appendChild(draggedBlock);
+  } else {
+    workspace.insertBefore(draggedBlock, afterElement);
+  }
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".ws-block:not(.dragging)")
+  ];
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child };
+    } else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
